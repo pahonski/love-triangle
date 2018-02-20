d36a4c16f7f2848ceec94effa7f715e8fa627350
Pavel <pahonski@gmail.com>
2018-02-20 16:02:47 +0300

    ready

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+    var counter = 0;
+
+    preferences.forEach(function (item, number, arr) {
+        var pack = [];
+        pack.push(item);
+        var flag = item;
+        if (item !== arr[item - 1]) {
+            pack.push(arr[item - 1]);
+            pack.push(arr[pack[pack.length-1]-1]);
+            pack.push(arr[pack[pack.length-1]-1]);
+            if (pack[pack.length-1] === flag) {
+                for (var i = 0; i < pack.length; i ++) {
+                    preferences[pack[i]-1] = undefined;
+                }
+                if (pack.every(function (t) {
+                        return t !== undefined;
+                    })) {
+                    console.log('Готовые ловеры');
+                    counter += 1;
+                }
+                console.log(pack);
+            }
+        } else {return}
+
+    });
+
+    return counter;
 };
